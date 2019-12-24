97f6018fb9babfbee0a96fcbcdcfe2834d92e899
Grant Jackson <gej1515@gmail.com>
2019-12-23 22:11:35 -0600

    Updates

---
+++ precache-manifest.420f01e1fcc34603a7324302f09b6c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddf344c0c989e6bcb141af3f48c17fc",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "47aa560b83c2703c9b22",
+    "url": "/Clicky/static/css/main.972edce9.chunk.css"
+  },
+  {
+    "revision": "3338b4237f5caaafbc31",
+    "url": "/Clicky/static/js/2.2145ea06.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky/static/js/2.2145ea06.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47aa560b83c2703c9b22",
+    "url": "/Clicky/static/js/main.4ce90124.chunk.js"
+  },
+  {
+    "revision": "7af722597c7d214f18cc",
+    "url": "/Clicky/static/js/runtime-main.b8f3e7d5.js"
+  }
+]);
